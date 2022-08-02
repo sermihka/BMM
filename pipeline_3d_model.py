155b3d4d8c29b53653b6cbce4d201172126561c1
Serg <medvedkoodom@gmail.com>
2022-08-02 16:24:20 +0700

    pipeline_model_1

--- pipeline_3d_model.py
+++ pipeline_3d_model.py
@@ -3,12 +3,6 @@ from copy import deepcopy
 import matplotlib.pyplot as plt
 import time
 
-# окно для отрисовки графика
-fig_2d = plt.figure(figsize=(7, 7))
-
-ax_2d = fig_2d.add_subplot()
-ax_2d.set_title("baze")
-
 # размеры массива
 ax0_max = 40
 ax1_max = 40
@@ -21,149 +15,167 @@ horizontal_ax1 = deepcopy(vertical)
 horizontal_ax2 = deepcopy(vertical)
 # vertical[ax0][ax1][ax2] ax0 - вертикаль
 
-# максимально возможное значение в трубочке
-TUBE_MAX = 1.0
-# минимально количество жидкости в трубочке
-# по вертикали после которого вода течёт
-DOWN_MIN = 0.1
-# процент от объёма воды, который утекает
-DOWN_PERS = 0.3
-# процент(от DOWN_PERS) который остаётся в этой ячейке, но переходит в горизонталь ax1
-DOWN_PERS_ax1 = 0.3
-# процент(от DOWN_PERS) который остаётся в этой ячейке, но переходит в горизонталь ax2
-DOWN_PERS_ax2 = 0.3
-# процент(от DOWN_PERS) который переходит в вертикаль ячейки, которая ниже
-DOWN_PERS_DOWN = 1.0 - DOWN_PERS_ax1 - DOWN_PERS_ax2
 
-# минимальноеколичество по горизонтали для вытекания
-LR_MIN = 0.01
-# Сколько вытекает в общем
-LR_PERS = 0.3
-# то, сколько перетекает в бок
-LR_PERS_L = 0.3
-LR_PERS_R = 0.3
-# то, сколько перетекает в вертикаль
-LR_PERS_DOWN = 1.0 - (LR_PERS_L + LR_PERS_R)
-
-# для проверки того, как долго работает прога
+# для проверки того, как долго работает цикл
 start_time = time.time()
+def cycle(N, vertical = vertical, horizontal_ax1 = horizontal_ax1, horizontal_ax2 = horizontal_ax2):
+    # максимально возможное значение в трубочке
+    TUBE_MAX = 1.0
+    # минимально количество жидкости в трубочке
+    # по вертикали после которого вода течёт
+    DOWN_MIN = 0.1
+    # процент от объёма воды, который утекает
+    DOWN_PERS = 0.3
+    # процент(от DOWN_PERS) который остаётся в этой ячейке, но переходит в горизонталь ax1
+    DOWN_PERS_ax1 = 0.3
+    # процент(от DOWN_PERS) который остаётся в этой ячейке, но переходит в горизонталь ax2
+    DOWN_PERS_ax2 = 0.3
+    # процент(от DOWN_PERS) который переходит в вертикаль ячейки, которая ниже
+    DOWN_PERS_DOWN = 1.0 - DOWN_PERS_ax1 - DOWN_PERS_ax2
 
-for n in range(300):
-    # типо жидкость капает
-    vertical[0][ax1_max//2][ax2_max//2] = 1
+    # минимальноеколичество по горизонтали для вытекания
+    LR_MIN = 0.01
+    # Сколько вытекает в общем
+    LR_PERS = 0.3
+    # то, сколько перетекает в бок
+    LR_PERS_L = 0.3
+    LR_PERS_R = 0.3
+    # то, сколько перетекает в вертикаль
+    LR_PERS_DOWN = 1.0 - (LR_PERS_L + LR_PERS_R)
 
-    # копируем массивы
-    ver = deepcopy(vertical)
-    hor_ax1 = deepcopy(horizontal_ax1)
-    hor_ax2 = deepcopy(horizontal_ax2)
+    for n in range(N):
+        # типо жидкость капает
+        vertical[0][ax1_max//2][ax2_max//2] = 1
 
+        # копируем массивы
+        ver = deepcopy(vertical)
+        hor_ax1 = deepcopy(horizontal_ax1)
+        hor_ax2 = deepcopy(horizontal_ax2)
 
-    for ax0 in range(ax0_max):
-        for ax1 in range(ax1_max):
-            for ax2 in range(ax2_max):
-                # проход по вертикальным трубочкам
-                if vertical[ax0][ax1][ax2] > DOWN_MIN:
-                    # то, сколько вытекает
-                    down = vertical[ax0][ax1][ax2] * DOWN_PERS
-                    # то, сколько перетекает в ось ax1
-                    down_ax1 = down*DOWN_PERS_ax1
-                    # то, сколько перетекает в ось ax2
-                    down_ax2 = down * DOWN_PERS_ax2
-                    # то, сколько перетекает в вертикаль нижней ячейки
-                    down_down = down * DOWN_PERS_DOWN
+        for ax0 in range(4 + n//50):
+            for ax1 in range((ax1_max//2 - (4 + n//50)), (4 + n//50 + ax1_max//2)):
+                for ax2 in range((ax2_max//2 - (4 + n//50)), (4 + n//50 + ax2_max//2)):
+                    # проход по вертикальным трубочкам
+                    if vertical[ax0][ax1][ax2] > DOWN_MIN:
+                        # то, сколько вытекает
+                        down = vertical[ax0][ax1][ax2] * DOWN_PERS
+                        # то, сколько перетекает в ось ax1
+                        down_ax1 = down*DOWN_PERS_ax1
+                        # то, сколько перетекает в ось ax2
+                        down_ax2 = down * DOWN_PERS_ax2
+                        # то, сколько перетекает в вертикаль нижней ячейки
+                        down_down = down * DOWN_PERS_DOWN
 
-                    # то, сколько может втечь в нужную трубочку
-                    tube_max_down = (TUBE_MAX - vertical[ax0 + 1][ax1][ax2])
-                    tube_max_ax1 = (TUBE_MAX - horizontal_ax1[ax0][ax1][ax2])
-                    tube_max_ax2 = (TUBE_MAX - horizontal_ax2[ax0][ax1][ax2])
+                        # то, сколько может втечь в нужную трубочку
+                        tube_max_down = (TUBE_MAX - vertical[ax0 + 1][ax1][ax2])
+                        tube_max_ax1 = (TUBE_MAX - horizontal_ax1[ax0][ax1][ax2])
+                        tube_max_ax2 = (TUBE_MAX - horizontal_ax2[ax0][ax1][ax2])
 
-                    if  (down_ax1 > tube_max_ax1):
-                        down_ax1 = tube_max_ax1
-                    if  (down_ax2 > tube_max_ax2):
-                        down_ax2 = tube_max_ax2
-                    if  (down_down > tube_max_down):
-                        down_down = tube_max_down
+                        if  (down_ax1 > tube_max_ax1):
+                            down_ax1 = tube_max_ax1
+                        if  (down_ax2 > tube_max_ax2):
+                            down_ax2 = tube_max_ax2
+                        if  (down_down > tube_max_down):
+                            down_down = tube_max_down
 
-                    down_stay = down - (down_down + down_ax1 + down_ax2)
+                        down_stay = down - (down_down + down_ax1 + down_ax2)
 
-                    ver[ax0][ax1][ax2] = vertical[ax0][ax1][ax2] - down + down_stay
-                    hor_ax1[ax0][ax1][ax2] = hor_ax1[ax0][ax1][ax2] + down_ax1
-                    hor_ax2[ax0][ax1][ax2] = hor_ax2[ax0][ax1][ax2] + down_ax2
-                    ver[ax0 + 1][ax1][ax2] = ver[ax0 + 1][ax1][ax2] + down_down
+                        ver[ax0][ax1][ax2] = vertical[ax0][ax1][ax2] - down + down_stay
+                        hor_ax1[ax0][ax1][ax2] = hor_ax1[ax0][ax1][ax2] + down_ax1
+                        hor_ax2[ax0][ax1][ax2] = hor_ax2[ax0][ax1][ax2] + down_ax2
+                        ver[ax0 + 1][ax1][ax2] = ver[ax0 + 1][ax1][ax2] + down_down
 
-                # проход по горизонтальным вдоль оси ax1
-                if horizontal_ax1[ax0][ax1][ax2] > LR_MIN:
+                    # проход по горизонтальным вдоль оси ax1
+                    if horizontal_ax1[ax0][ax1][ax2] > LR_MIN:
 
-                    spread = horizontal_ax1[ax0][ax1][ax2] * LR_PERS
-                    # перетикает вправо вдоль оси ax1
-                    spread_r = spread * LR_PERS_R
-                    # перетикает влево вдоль оси ax1
-                    spread_l = spread * LR_PERS_L
-                    # перетекает в вертикаль
-                    spread_d = spread * LR_PERS_DOWN
+                        spread = horizontal_ax1[ax0][ax1][ax2] * LR_PERS
+                        # перетикает вправо вдоль оси ax1
+                        spread_r = spread * LR_PERS_R
+                        # перетикает влево вдоль оси ax1
+                        spread_l = spread * LR_PERS_L
+                        # перетекает в вертикаль
+                        spread_d = spread * LR_PERS_DOWN
 
-                    # дальше, по сути, всё аналогично вретикальному растеканию
+                        # дальше, по сути, всё аналогично вретикальному растеканию
 
-                    tube_max_sp_r = TUBE_MAX - horizontal_ax1[ax0][ax1 + 1][ax2]
-                    tube_max_sp_l = TUBE_MAX - horizontal_ax1[ax0][ax1 - 1][ax2]
-                    tube_max_sp_d = TUBE_MAX - vertical[ax0][ax1][ax2]
+                        tube_max_sp_r = TUBE_MAX - horizontal_ax1[ax0][ax1 + 1][ax2]
+                        tube_max_sp_l = TUBE_MAX - horizontal_ax1[ax0][ax1 - 1][ax2]
+                        tube_max_sp_d = TUBE_MAX - vertical[ax0][ax1][ax2]
 
-                    if spread_d > tube_max_sp_d:
-                        spread_d = tube_max_sp_d
-                    if spread_l > tube_max_sp_l:
-                        spread_l = tube_max_sp_l
-                    if spread_r > tube_max_sp_r:
-                        spread_r = tube_max_sp_r
+                        if spread_d > tube_max_sp_d:
+                            spread_d = tube_max_sp_d
+                        if spread_l > tube_max_sp_l:
+                            spread_l = tube_max_sp_l
+                        if spread_r > tube_max_sp_r:
+                            spread_r = tube_max_sp_r
 
-                    spread_stay = spread - (spread_d + spread_r + spread_l)
+                        spread_stay = spread - (spread_d + spread_r + spread_l)
 
-                    hor_ax1[ax0][ax1][ax2] = hor_ax1[ax0][ax1][ax2] - spread + spread_stay
-                    hor_ax1[ax0][ax1 + 1][ax2] = hor_ax1[ax0][ax1 + 1][ax2] + spread_r
-                    hor_ax1[ax0][ax1 - 1][ax2] = hor_ax1[ax0][ax1 - 1][ax2] + spread_l
-                    ver[ax0][ax1][ax2] = ver[ax0][ax1][ax2] + spread_d
+                        hor_ax1[ax0][ax1][ax2] = hor_ax1[ax0][ax1][ax2] - spread + spread_stay
+                        hor_ax1[ax0][ax1 + 1][ax2] = hor_ax1[ax0][ax1 + 1][ax2] + spread_r
+                        hor_ax1[ax0][ax1 - 1][ax2] = hor_ax1[ax0][ax1 - 1][ax2] + spread_l
+                        ver[ax0][ax1][ax2] = ver[ax0][ax1][ax2] + spread_d
 
-                # проход по горизонтальным вдоль оси ax2
-                if horizontal_ax2[ax0][ax1][ax2] > LR_MIN:
-                    ax2spread = horizontal_ax2[ax0][ax1][ax2] * LR_PERS
-                    # перетикает вправо вдоль оси ax1
-                    ax2spread_r = ax2spread * LR_PERS_R
-                    # перетикает влево вдоль оси ax1
-                    ax2spread_l = ax2spread * LR_PERS_L
-                    # перетекает в вертикаль
-                    ax2spread_d = ax2spread * LR_PERS_DOWN
+                    # проход по горизонтальным вдоль оси ax2
+                    if horizontal_ax2[ax0][ax1][ax2] > LR_MIN:
+                        ax2spread = horizontal_ax2[ax0][ax1][ax2] * LR_PERS
+                        # перетикает вправо вдоль оси ax1
+                        ax2spread_r = ax2spread * LR_PERS_R
+                        # перетикает влево вдоль оси ax1
+                        ax2spread_l = ax2spread * LR_PERS_L
+                        # перетекает в вертикаль
+                        ax2spread_d = ax2spread * LR_PERS_DOWN
 
-                    ax2tube_max_sp_r = TUBE_MAX - horizontal_ax2[ax0][ax1][ax2 + 1]
-                    ax2tube_max_sp_l = TUBE_MAX - horizontal_ax2[ax0][ax1][ax2 - 1]
-                    ax2tube_max_sp_d = TUBE_MAX - vertical[ax0][ax1][ax2]
+                        ax2tube_max_sp_r = TUBE_MAX - horizontal_ax2[ax0][ax1][ax2 + 1]
+                        ax2tube_max_sp_l = TUBE_MAX - horizontal_ax2[ax0][ax1][ax2 - 1]
+                        ax2tube_max_sp_d = TUBE_MAX - vertical[ax0][ax1][ax2]
 
-                    if ax2spread_d > ax2tube_max_sp_d:
-                        ax2spread_d = ax2tube_max_sp_d
-                    if ax2spread_l > ax2tube_max_sp_l:
-                        ax2spread_l = ax2tube_max_sp_l
-                    if ax2spread_r > ax2tube_max_sp_r:
-                        ax2spread_r = ax2tube_max_sp_r
+                        if ax2spread_d > ax2tube_max_sp_d:
+                            ax2spread_d = ax2tube_max_sp_d
+                        if ax2spread_l > ax2tube_max_sp_l:
+                            ax2spread_l = ax2tube_max_sp_l
+                        if ax2spread_r > ax2tube_max_sp_r:
+                            ax2spread_r = ax2tube_max_sp_r
 
-                    ax2spread_stay = ax2spread - (ax2spread_d + ax2spread_r + ax2spread_l)
+                        ax2spread_stay = ax2spread - (ax2spread_d + ax2spread_r + ax2spread_l)
 
-                    hor_ax2[ax0][ax1][ax2] = horizontal_ax2[ax0][ax1][ax2] - ax2spread + ax2spread_stay
-                    hor_ax2[ax0][ax1][ax2 + 1] = hor_ax2[ax0][ax1][ax2 + 1] + ax2spread_r
-                    hor_ax2[ax0][ax1][ax2 - 1] = hor_ax2[ax0][ax1][ax2 - 1] + ax2spread_l
-                    ver[ax0][ax1][ax2] = ver[ax0][ax1][ax2] + ax2spread_d
+                        hor_ax2[ax0][ax1][ax2] = horizontal_ax2[ax0][ax1][ax2] - ax2spread + ax2spread_stay
+                        hor_ax2[ax0][ax1][ax2 + 1] = hor_ax2[ax0][ax1][ax2 + 1] + ax2spread_r
+                        hor_ax2[ax0][ax1][ax2 - 1] = hor_ax2[ax0][ax1][ax2 - 1] + ax2spread_l
+                        ver[ax0][ax1][ax2] = ver[ax0][ax1][ax2] + ax2spread_d
 
-    # опять перезаписываем массивы
-    vertical = deepcopy(ver)
-    horizontal_ax1 = deepcopy(hor_ax1)
-    horizontal_ax2 = deepcopy(hor_ax2)
-# выводит время работы проги
+        # опять перезаписываем массивы
+        vertical = deepcopy(ver)
+        horizontal_ax1 = deepcopy(hor_ax1)
+        horizontal_ax2 = deepcopy(hor_ax2)
+    return vertical, horizontal_ax1, horizontal_ax2
+# выводит время работы цикла
 print('Finished cycle in %s seconds' % (time.time() - start_time))
 
-# ну а тут у нас по сути разрез берётся
-result = np.array([[0. for i in range(ax0_max)] for j in range(ax1_max)])
-for ax0 in range(ax0_max):
-    for ax1 in range(ax1_max):
-        result[ax0][ax1] = vertical[ax0][ax1][ax2_max//2]
 
-# отображение градиента
-ax_2d.imshow(result)
+def graph(result):
+    # окно для отрисовки графика
+    fig_2d = plt.figure(figsize=(7, 7))
 
-plt.show()
+    ax_2d = fig_2d.add_subplot()
+    ax_2d.set_title("baze")
+
+    # отображение градиента
+    ax_2d.imshow(result)
+
+    plt.show()
+
+def incision(v, h1, h2):
+    # ну а тут у нас по сути разрез берётся
+    result = np.array([[0. for f in range(ax0_max)] for g in range(ax1_max)])
+    for ax0 in range(ax0_max):
+        for ax1 in range(ax1_max):
+            result[ax0][ax1] = v[ax0][ax1][ax2_max//2] + h1[ax0][ax1][ax2_max//2] + h2[ax0][ax1][ax2_max//2]
+    return result
+
+# v, h1, h2 = cycle(400)
+# вызов разреза
+# RES = incision(v, h1, h2)
+# вызов графика
+# graph(RES)
+
--- win_graf.py
+++ win_graf.py
@@ -1,68 +1,57 @@
-#Окно три столбца, первый столбец выбрать жидкость , пока что только вода, второй столбец выбор почвы , пока что только песок , последний столбец кнопка старт (начало)
-from operator import truediv
+"""
+Окно три столбца, первый столбец выбрать жидкость , пока что только вода, второй столбец выбор почвы ,
+пока что только песок , последний столбец кнопка старт (начало)
+"""
 import tkinter as tk
-choice_soil ='Ничего'
-choice_liquid='Ничего'
-water_p='вода'
-oil_p='нейть'
-butter_p='масло'
-sand_p='песок'
-clay_p='глина'
-dert_p='земля'
+import pipeline_3d_model
+choice_soil = ''
+choice_liquid = ''
+
+# функция вызванная кнопкой воды
 def water():
-    print('вы выбрали воду')
     global choice_liquid
-    choice_liquid='Вода'
-def oil():
-    print('вы выбрали нефть')
-    global choice_liquid
-    choice_liquid='Нефть'
-def butter():
-    print('вы выбрали масло')
-    global choice_liquid
-    choice_liquid='Масло'   
+    choice_liquid = 'water'
+
+# функция вызванная кнопкой "sand"
 def sand():
-    print('вы выбрали песок')
     global choice_soil
-    choice_soil='Песок'
-def clay():
-    print('вы выбрали глину')
-    global choice_soil
-    choice_soil='Глину'
-def dert():
-    print('вы выбрали землю')
-    global choice_soil
-    choice_soil='Землю'
+    choice_soil = 'sand'
+
+# функция вызываемая кнопкой "liquid"
 def liquid():
-    global liquid
-    liquid=''
-    tk.Button(win,text='water',command=water,activebackground='blue').grid(row=3,column=0,stick='we')
-    tk.Button(win,text='oil',command=oil,activebackground='black').grid(row=4,column=0,stick='we')
-    tk.Button(win,text='butter',command=butter,activebackground='yellow').grid(row=5,column=0,stick='we')
+    tk.Button(win, text='water', command=water, activebackground='blue').grid(row=3, column=0, stick='we')
+# функция вызываемая кнопкой "soil"
 def soil():
-    global soil
-    soil = ''
-    tk.Button(win,text='sand',command=sand,activebackground='yellow').grid(row=3, column=1, stick='we')
-    tk.Button(win,text='clay',command=clay,activebackground='brown').grid(row=4, column=1, stick='we')
-    tk.Button(win,text='dert',command=dert,activebackground='brown').grid(row=5, column=1, stick='we')
+    tk.Button(win, text='sand', command=sand, activebackground='yellow').grid(row=3, column=1, stick='we')
+
 def start():
     global choice_liquid
-    print('Вы выбрали условия жидкости', choice_liquid, 'и условие почвы', choice_soil)
+    global choice_soil
+    if choice_soil == 'sand' and choice_liquid == 'water':
+        v, h1, h2 = pipeline_3d_model.cycle(300)
+        RES = pipeline_3d_model.incision(v, h1, h2)
+        pipeline_3d_model.graph(RES)
+
 win = tk.Tk()
-#photo=tk.PhotoImage(file='D:\БММ\python графика уроки/62881.png')
-#win.iconphoto(False, photo)
+
+# интерфейс окна
 win.config(bg='#55E0FF')
 win.title('Матиматическое моделирование процесса просацивания жидкости в пористые среды')
+# размеры
 win.geometry("800x400+100+200")
-win.minsize(300,400)
-win.maxsize(700,800)
-win.resizable(False,False)
-tk.Label(win,text='Выберите жидкость',bg='#43FF91',font=('Arial',10,'bold'),width=15,height=5,).grid(row=0,column=0,stick='we')
-tk.Label(win,text='Выберите породу',bg='#43FF91',font=('Arial',10,'bold'),width=15,height=5,).grid(row=0,column=1,stick='we')
-tk.Label(win,text='Нажмите для вывода',bg='#43FF91',font=('Arial',10,'bold'),width=15,height=5,justify=tk.CENTER).grid(row=0,column=2,stick='we')
-tk.Button(win,text='liquid',command=liquid).grid(row=1,column=0,stick='we',rowspan=(2))
-tk.Button(win,text='soil',command=soil).grid(row=2,column=1,stick='we')
-tk.Button(win,text='start',command=start).grid(row=3,column=2)
+win.minsize(300, 400)
+win.maxsize(700, 800)
+# запрет на именения размера окна
+win.resizable(False, False)
+# текстовые надписи
+tk.Label(win, text='Выберите жидкость', bg='#43FF91', font=('Arial', 10, 'bold'), width=15, height=5,).grid(row=0, column=0, stick='we')
+tk.Label(win, text='Выберите породу', bg='#43FF91', font=('Arial', 10, 'bold'), width=15, height=5,).grid(row=0, column=1, stick='we')
+tk.Label(win, text='Нажмите для вывода', bg='#43FF91', font=('Arial', 10, 'bold'), width=15, height=5, justify=tk.CENTER).grid(row=0, column=2, stick='we')
+# кнопки
+tk.Button(win, text='liquid', command=liquid).grid(row=1, column=0, stick='we', rowspan=(2))
+tk.Button(win, text='soil', command=soil).grid(row=2, column=1, stick='we')
+tk.Button(win, text='start', command=start).grid(row=3, column=2)
+# минимальные размеры колонок
 win.grid_columnconfigure(0, minsize=250)
 win.grid_columnconfigure(1, minsize=250)
 win.grid_columnconfigure(2, minsize=200)
